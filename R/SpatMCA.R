3fbd819324a90279d85089f9061154cb46ec884c
Wen-Ting Wang <egpivo@gmail.com>
2017-01-26 13:48:49 +0800

    Update spatmca

--- R/SpatMCA.R
+++ R/SpatMCA.R
@@ -32,11 +32,12 @@ spatmca <- function(x1, x2, Y1, Y2, M = 5, K = NULL, K.select = ifelse(is.null(K
   if(is.null(tau2u)&&is.null(tau2v)){
     ntau2u <- ntau2v <- 11
     
-    indexu <- sort(abs(tempegvl3$u[,1]),decreasing=T,index.return=T)$ix
+    indexu <- sort(abs(tempegvl3$u[,1]), decreasing=T, index.return=T)$ix
     nu1u <- indexu[2]
     nu2u <- indexu[ncol(Y1)]
     max.tau2u <- 2*abs(dd[nu1u,]%*%tempegvl3$v[,1])
     min.tau2u <- abs(dd[nu2u,]%*%tempegvl3$v[,1])
+  
     tau2u <- c(0,exp(seq(log(min.tau2u), log(max.tau2u), length = (ntau2u-1)))) 
     
     indexv <- sort(abs(tempegvl3$v[,1]),decreasing=T,index.return=T)$ix
