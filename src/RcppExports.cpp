ee90b80494f72407c960db46051cd65a96e18544
Joseph (Wen-Ting) Wang <egpivo@gmail.com>
2023-01-31 20:19:44 +0800

    [BUGFIX] Fix memory leak on some platforms (#11)
    * CHG: remove the C++ flag to use the default compiler; define the RCPP_PARALLEL_BACKEND backend
    
    * FIX: correct the outdated urls
    
    ---------
    
    Co-authored-by: joseph.wangwt <joseph.wangwt@shopee.com>

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // tpm2
 arma::mat tpm2(const arma::mat z, const arma::mat P, const arma::mat Phi);
 RcppExport SEXP _SpatMCA_tpm2(SEXP zSEXP, SEXP PSEXP, SEXP PhiSEXP) {
