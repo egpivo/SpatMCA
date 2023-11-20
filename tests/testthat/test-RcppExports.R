e06569df2b7e317bfc6294dcea6843d0b9a3af19
egpivo <egpivo@gmail.com>
2023-11-20 09:56:53 +0800

    CHG: add unit tests of

--- tests/testthat/test-RcppExports.R
+++ tests/testthat/test-RcppExports.R
@@ -34,10 +34,6 @@ test_that("spatmcacvall_rcpp returns the correct result", {
   expect_true("cvtau2u" %in% names(result))
   expect_true("cvtau1v" %in% names(result))
   expect_true("cvtau2v" %in% names(result))
-  
-  # Add more specific checks based on the expected output
-  # ...
-  
 })
 
 test_that("tpm2 function produces correct output", {
@@ -51,3 +47,43 @@ test_that("tpm2 function produces correct output", {
   
   expect_equal(dim(result), c(nrow(z), ncol(Phi)))
 })
+
+
+# Create a unit test
+test_that("spatmcacv_rcpp function produces correct output", {
+  # Set up test data (replace this with your actual test data)
+  sxr <- matrix(rnorm(100), nrow = 10, ncol = 10)
+  syr <- matrix(rnorm(100), nrow = 10, ncol = 10)
+  Xr <- matrix(rnorm(100), nrow = 10, ncol = 10)
+  Yr <- matrix(rnorm(100), nrow = 10, ncol = 10)
+  M <- 5
+  K <- 3
+  tau1ur <- seq(0.1, 1, length.out = 5)
+  tau2ur <- seq(0.1, 1, length.out = 5)
+  tau1vr <- seq(0.1, 1, length.out = 5)
+  tau2vr <- seq(0.1, 1, length.out = 5)
+  nkr <- seq(1, 5)
+  maxit <- 100
+  tol <- 1e-5
+  l2ur <- seq(0.1, 1, length.out = 5)
+  l2vr <- seq(0.1, 1, length.out = 5)
+  
+  # Call the spatmcacv_rcpp function
+  result <- spatmcacv_rcpp(sxr, syr, Xr, Yr, M, K, tau1ur, tau2ur, tau1vr, tau2vr, nkr, maxit, tol, l2ur, l2vr)
+  
+  
+  # Check if the result is a list
+  expect_is(result, "list")
+  
+  # Check if the list contains expected components
+  expect_true("cv1" %in% names(result))
+  expect_true("cv2" %in% names(result))
+  expect_true("Uest" %in% names(result))
+  expect_true("Vest" %in% names(result))
+  expect_true("Dest" %in% names(result))
+  expect_true("cvtau1u" %in% names(result))
+  expect_true("cvtau2u" %in% names(result))
+  expect_true("cvtau1v" %in% names(result))
+  expect_true("cvtau2v" %in% names(result))
+})
+
